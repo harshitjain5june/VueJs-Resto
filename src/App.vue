36939621b6516239d9065322e35927a34cf341e5
Harshit Jain <harshitjain5june@gmail.com>
2023-04-29 03:02:29 +0530

    full project

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+ 
+<router-view />
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.logo{
+    width: 300px;
+}
+.form{
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+    margin-left: auto;
+    margin-right: auto;
+    gap: 1rem;
+    
+}
+.items{
+    height: 4vh;
+}
+</style>
